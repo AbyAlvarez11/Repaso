268002538892ca712eb36f7aff5238b1f83308a8
Antony Daniel Rivera Colan <alcom13000@gmail.com>
2022-12-29 21:47:32 -0500

    Inicial 2

--- src/prueba/Prueba2.java
+++ src/prueba/Prueba2.java
@@ -6,7 +6,7 @@ public class Prueba2 {
         int i = 0;
 
         do {
-            i = (int) ((600 - 200 + 1) * Math.random() + 200);
+            i = (int) ((900 - 300 + 1) * Math.random() + 200);
 
             System.out.println("El numero es --> " + i ); // Bucle infinito.
         } while(!(i == 300));//true esto continua
