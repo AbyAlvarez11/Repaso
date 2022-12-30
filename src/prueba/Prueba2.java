4fd6d33a0eb77ebacbce0544b8c4ef9276b72564
AbyAlvarez11 <121649242+AbyAlvarez11@users.noreply.github.com>
2022-12-29 21:46:03 -0500

    Inicial2
    Inicial2

--- src/prueba/Prueba2.java
+++ src/prueba/Prueba2.java
@@ -8,7 +8,7 @@ public class Prueba2 {
         do {
             i = (int) ((600 - 200 + 1) * Math.random() + 200);
 
-            System.out.println("El numero es -> " + i ); // Bucle infinito.
+            System.out.println("El numero es --> " + i ); // Bucle infinito.
         } while(!(i == 300));//true esto continua
 
     }
